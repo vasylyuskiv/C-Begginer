2e5d7993bc6b7c87d32dbf1089f428d83ed231e7
vasylyuskiv <uzykday@gmail.com>
2017-09-08 14:03:28 +0300

    Add files via upload

---
+++ H3Task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+
+        { //Homework 3 Task1
+            int LichA = 0;
+            int LichO = 0;
+            int LichE = 0;
+            int LichI = 0;
+            Console.WriteLine("Enter your text here");
+
+            string s = Console.ReadLine();
+            foreach (Char a in s)
+            {
+                if (a == 'a')
+                {
+                    LichA++;
+
+                }
+                if (a == 'i')
+                {
+                    LichI++;
+                }
+                if (a == 'e')
+                {
+                    LichE++;
+                }
+                if (a == 'o') { LichO++; }
+            }
+            Console.WriteLine("Letters A in text {0}, Letters I in text {1}, Letters E in text {2}, Letters O in text {3}", LichA, LichI, LichE, LichO);
+        }
+    }
+}
---
+++ H3Task2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] longmonth = new int[7] { 1, 3, 5, 7, 8, 10, 12 };
+            int[] shortmonth = new int[4] { 4, 6, 9, 11 };
+            int[] feb = new int[1] { 2 };
+
+
+            Console.WriteLine("Hello, Please, enter the number of the month");
+            int mon = int.Parse(Console.ReadLine());
+
+            foreach (int x in longmonth)
+            {
+                if (x == mon)
+                {
+                    Console.WriteLine("In this month 31 days");
+                }
+
+            }
+            foreach (int y in shortmonth)
+            {
+                if (y == mon)
+                {
+                    Console.WriteLine("In this month 30 days");
+                }
+            }
+            foreach (int z in feb)
+            {
+                if (z == mon)
+                {
+                    Console.WriteLine("In this month 28 or 29 days");
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
+
