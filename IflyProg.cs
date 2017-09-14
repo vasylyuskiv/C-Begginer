148177eff71119a23585f07c18258c58c5cd5c1d
vasylyuskiv <uzykday@gmail.com>
2017-09-14 11:06:37 +0300

    Add files via upload

---
+++ IflyProg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Iflyable> list = new List<Iflyable>();
+            list.Add(new Bird());
+            list.Add(new Bird());
+            list.Add(new Plane());
+            list.Add(new Plane());
+
+            foreach (var c in list)
+                c.fly();
+
+
+        }
+    }
+}
