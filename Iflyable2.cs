a61bb0b023d2464aee9e8246f2bc0cb59ed2949b
vasylyuskiv <uzykday@gmail.com>
2017-09-14 11:06:18 +0300

    Add files via upload

---
+++ Iflyable2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    interface Iflyable
+    {
+        void fly();
+
+    }
+    public class Bird : Iflyable
+    {
+        private string name;
+        private bool canFly = true;
+
+        public void fly()
+        {
+            if (canFly)
+            Console.WriteLine("i'm bird {0} i can fly", name);
+            else
+                Console.WriteLine("i'm bird {0} i can't fly", name);
+
+        }
+    }
+    public class Plane : Iflyable
+    {
+        private string mark="boeing";
+        private int highFly=5;
+
+        public void fly()
+        {
+            if (highFly>3)
+                Console.WriteLine("i'm plane {0} i can fly high", mark);
+            else
+                Console.WriteLine("i'm bird {0} im flying not so high", mark);
+
+        }
+    }
+}
+
