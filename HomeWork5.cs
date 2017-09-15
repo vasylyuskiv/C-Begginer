f7f9c3efceb3a6e287641c1c0d5ee529e11c5321
vasylyuskiv <uzykday@gmail.com>
2017-09-15 14:53:56 +0300

    Add files via upload

---
+++ HomeWork5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<IDeveloper> list = new List<IDeveloper>();
+            list.Add(new Programmer("C++"));
+            list.Add(new Builder("hummer"));
+
+            foreach (var c in list)
+                c.create();
+            foreach (var d in list)
+                d.destroy();
+
+            //Programmer programmer = new Programmer("c++");
+            //programmer.create();
+            //programmer.destroy();
+
+            //Builder builder = new Builder("hummer");
+            //builder.create();
+            //builder.destroy();
+
+
+
+
+        }
+    }
+}
