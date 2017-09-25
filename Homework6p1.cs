45f51cf47f7eaac85527da88e18a0c07752c1738
vasylyuskiv <uzykday@gmail.com>
2017-09-25 15:36:39 +0300

    Add files via upload

---
+++ Homework6p1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Dictionary<String, long> PhoneBook = new Dictionary<string, long>();
+            StreamReader sr = new StreamReader(@"C:\Users\Vasyl\Documents\Visual Studio 2017\Projects\ConsoleApp10\ConsoleApp10\Phones.txt");
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+
+                string[] splitted = line.Split(',');
+                string name = splitted[0].Trim();
+                long tel = long.Parse(splitted[1].Trim());
+
+                PhoneBook.Add(name, tel);
+                foreach (KeyValuePair<string, long> kvp in PhoneBook)
+                    Console.WriteLine("User = {0}, number = {1}", kvp.Key, kvp.Value);
+
+
+
+                using (StreamWriter PhO = new StreamWriter("PhonesOnly.txt"))
+                    foreach (KeyValuePair<string, long> kvp in PhoneBook)
+                        PhO.WriteLine("{0}", kvp.Value);
+
+                using (StreamWriter newPh = new StreamWriter("New.txt"))
+                    foreach (KeyValuePair<string, long> kvp in PhoneBook)
+                        newPh.WriteLine("+3{0}", kvp.Value);
+
+
+
+            }
+
+            sr.Close();
+
+            Console.WriteLine("hello please enter Name of the person what are u looking for");
+            string ln = Console.ReadLine();
+
+            if (ln != null)
+            {
+                Console.WriteLine(PhoneBook[ln]);
+            }
+            else
+            {
+                //тут тре зробити ще 
+                Console.WriteLine("there is no such Person in our base");
+
+            }
+
+        }
+    }
+}
---
+++ Homework6p2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6p2
+{
+    class Program
+    {
+        static int ReadNumber(int start, int end, int p)
+        {
+
+
+            int a = Int32.Parse(Console.ReadLine());
+
+            if ((a < start) || (a > end))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int p = 1; p <= 10; p++)
+                {
+                    Console.Write("Enter number in the rangefrom 1 to 10 : ");
+                    int a = ReadNumber(1, 100, p);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The number is NOT integer!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Null is NOT integer.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The number is NOT in the range of integer.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("The number is NOT in the range.");
+            }
+
+
+
+        }
+    }
+}
+
+
